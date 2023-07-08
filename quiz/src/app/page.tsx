2e8e2c41f2a907ea751dd220c3c4b0cee764298b
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2023-07-07 23:37:10 -0300

    Iniciando o projeto de quiz

---
+++ quiz/src/app/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+export default function Home() {
+  return (
+    <div className="flex flex-col bg-sky-700 w-screen h-screen justify-center items-center">
+      <div className="bg-white h-80 w-80">
+      <div className="text-black">Quiz de culinária</div>
+      <div className="text-black">Qual ingrediente nào pode faltar?</div>
+      <div className="text-black">Resposta 1</div>
+      <div className="text-black">Resposta 2</div>
+      <div className="text-black">Resposta 3</div>
+      <div className="text-black">Resposta 4</div>
+      <div className="text-black">Pergunta 1 de 1</div>
+      </div>
+    </div>
+  )
+}
