afcdd829cb417b243b4ce97cd9a06c500c0768bd
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2023-07-08 15:14:06 -0300

    Nao consegui passar disso
    queria uma maneira mais "facil" de passar o parametro "key" para uma ação de clicar o botao pra saber qual div é, porem a solução passa dos meus conhecimentos no momento

--- quiz/src/components/Respostas.tsx
+++ quiz/src/components/Respostas.tsx
@@ -3,18 +3,24 @@ import { EstruturaRespostas } from "@/types/EstruturaRespostas"
 export const Respostas = (EstruturaRespostas: EstruturaRespostas) => {
     
     
-    const clicouBotao = () => {
-        alert("clicou")
+    const clicouBotao = (chave) => {
+        //let lista = EstruturaRespostas.ListaRespostas.ListadeResposta.map(e => e.VouF)
+        if (chave){
+            //console.log(chave)
+        }
     }
     
     return(
         <div className=" text-black">
             <div className="text-black text-3xl p-5">{EstruturaRespostas.Pergunta}</div>
             <div>{EstruturaRespostas.ListaRespostas.ListadeResposta.map(
-                item => <div 
-                onClick={clicouBotao}
-                className="bg-blue-100 p-3 m-3 rounded-md border border-blue-400"> 
-                {item.Resposta} </div>)}
+                (item, key) => <div 
+                key={key}
+                onClick={clicouBotao(key)}
+                className="bg-blue-100 p-3 m-3 rounded-md border border-blue-400 cursor-pointer"
+                > 
+                {item.Resposta} {key}
+                </div>)}
             </div>
         </div>
         
